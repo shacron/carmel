9e06bb4af69b89e03400e8237f345d63f5ab2779
Shac Ron <sron@nvidia.com>
2022-09-15 16:09:02 -0700

    Remove redundant setting of tools

--- target/darwin.mk
+++ target/darwin.mk
@@ -13,10 +13,10 @@ ARCHIVE := $(shell xcrun -sdk $(SDK) -find ar)
 LINK := $(shell xcrun -sdk $(SDK) -find ld)
 # MINVER := -macosx_version_min 12.1
 
-CC := clang
-CXX := clang++
-ARCHIVE := ar
-LINK := ld
+# CC := clang
+# CXX := clang++
+# ARCHIVE := ar
+# LINK := ld
 
 TEST_INCLUDES += -I$(UROOT)/Cellar/googletest/1.12.0/include
 LINKFLAGS := -L$(SYSLIBS) -L$(UROOT)/lib
