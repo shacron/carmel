624681264026e08057d26dd408b24c2a1d530747
Shac Ron <sron@nvidia.com>
2022-09-23 16:31:25 -0700

    Remove stdint_clang.h

--- include/stdint.h
+++ include/stdint.h
@@ -2,10 +2,91 @@
 
 #if __clang__
 
-#include "_inc/stdint_clang.h"
+typedef __INT8_TYPE__   int8_t;
+typedef __INT16_TYPE__  int16_t;
+typedef __INT32_TYPE__  int32_t;
+typedef __INT64_TYPE__  int64_t;
+typedef __UINT8_TYPE__  uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+typedef __UINT32_TYPE__ uint32_t;
+typedef __UINT64_TYPE__ uint64_t;
 
-#else
+typedef __INT_LEAST8_TYPE__   int_least8_t;
+typedef __INT_LEAST16_TYPE__  int_least16_t;
+typedef __INT_LEAST32_TYPE__  int_least32_t;
+typedef __INT_LEAST64_TYPE__  int_least64_t;
+typedef __UINT_LEAST8_TYPE__  uint_least8_t;
+typedef __UINT_LEAST16_TYPE__ uint_least16_t;
+typedef __UINT_LEAST32_TYPE__ uint_least32_t;
+typedef __UINT_LEAST64_TYPE__ uint_least64_t;
+
+typedef __INT_FAST8_TYPE__   int_fast8_t;
+typedef __INT_FAST16_TYPE__  int_fast16_t;
+typedef __INT_FAST32_TYPE__  int_fast32_t;
+typedef __INT_FAST64_TYPE__  int_fast64_t;
+typedef __UINT_FAST8_TYPE__  uint_fast8_t;
+typedef __UINT_FAST16_TYPE__ uint_fast16_t;
+typedef __UINT_FAST32_TYPE__ uint_fast32_t;
+typedef __UINT_FAST64_TYPE__ uint_fast64_t;
+
+typedef __INTPTR_TYPE__  intptr_t;
+typedef __UINTPTR_TYPE__ uintptr_t;
+
+typedef __INTMAX_TYPE__  intmax_t;
+typedef __UINTMAX_TYPE__ uintmax_t;
+
+#define INT8_MAX   __INT8_MAX__
+#define INT16_MAX  __INT16_MAX__
+#define INT32_MAX  __INT32_MAX__
+#define INT64_MAX  __INT64_MAX__
+#define UINT8_MAX  __UINT8_MAX__
+#define UINT16_MAX __UINT16_MAX__
+#define UINT32_MAX __UINT32_MAX__
+#define UINT64_MAX __UINT64_MAX__
+
+#define INT_LEAST8_MAX   __INT_LEAST8_MAX__
+#define INT_LEAST16_MAX  __INT_LEAST16_MAX__
+#define INT_LEAST32_MAX  __INT_LEAST32_MAX__
+#define INT_LEAST64_MAX  __INT_LEAST64_MAX__
+#define UINT_LEAST8_MAX  __UINT_LEAST8_MAX__
+#define UINT_LEAST16_MAX __UINT_LEAST16_MAX__
+#define UINT_LEAST32_MAX __UINT_LEAST32_MAX__
+#define UINT_LEAST64_MAX __UINT_LEAST64_MAX__
+
+#define INT_FAST8_MAX   __INT_FAST8_MAX__
+#define INT_FAST16_MAX  __INT_FAST16_MAX__
+#define INT_FAST32_MAX  __INT_FAST32_MAX__
+#define INT_FAST64_MAX  __INT_FAST64_MAX__
+#define UINT_FAST8_MAX  __UINT_FAST8_MAX__
+#define UINT_FAST16_MAX __UINT_FAST16_MAX__
+#define UINT_FAST32_MAX __UINT_FAST32_MAX__
+#define UINT_FAST64_MAX __UINT_FAST64_MAX__
+
+#define INTPTR_MAX  __INTPTR_MAX__
+#define PTRDIFF_MAX __PTRDIFF_MAX__
+#define SIG_ATOMIC_MAX __SIG_ATOMIC_MAX__
+#define SIZE_MAX  __SIZE_MAX__
+#define WCHAR_MAX __WCHAR_MAX__
+#define WINT_MAX  __WINT_MAX__
+
+#else // clang
 
 #error unknown compiler
 
 #endif
+
+#define PTRDIFF_MIN (ptrdiff_t)(PTRDIFF_MAX + 1)
+#define SIG_ATOMIC_MIN (sig_atomic_t)(SIG_ATOMIC_MAX + 1)
+#define WCHAR_MIN (wchar_t)(WCHAR_MAX + 1)
+
+#define INT8_C(v)   (int8_t)(v)
+#define INT16_C(v)  (int16_t)(v)
+#define INT32_C(v)  (int32_t)(v)
+#define INT64_C(v)  (int64_t)(v)
+#define UINT8_C(v)  (uint8_t)(v)
+#define UINT16_C(v) (uint16_t)(v)
+#define UINT32_C(v) (uint32_t)(v)
+#define UINT64_C(v) (uint64_t)(v)
+
+#define INTMAX_C(v)  (intmax_t)(v)
+#define UINTMAX_C(v) (uintmax_t)(v)
