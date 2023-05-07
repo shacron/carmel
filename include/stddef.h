e08ab191473bf7e4bfac34e5a01e49fc258bb3a8
Shac Ron <shac.ron@gmail.com>
2023-05-07 16:33:04 -0700

    ptrdiff_t

--- include/stddef.h
+++ include/stddef.h
@@ -9,4 +9,10 @@ typedef __SIZE_TYPE__ size_t;
 typedef unsigned long size_t;
 #endif
 
+#ifdef __PTRDIFF_TYPE__
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#else
+typedef unsigned long ptrdiff_t;
+#endif
+
 #define offsetof(t,m) __builtin_offsetof(t,m)
