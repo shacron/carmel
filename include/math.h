a50b76065482fbba1d6f5252c03ab74b0065f039
Shac Ron <shac.ron@gmail.com>
2024-09-24 21:01:46 -0700

    Preliminary math.h

---
+++ include/math.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define M_PI    3.14159265358979323846264338327950288
+#define M_PI_2  1.57079632679489661923132169163975144
+#define M_PI_4  0.785398163397448309615660845819875721
+
+#if CARMEL_FLOAT
+float sinf(float x);
+double sin(double x);
+#endif
